347a50f155198c1804e545ee461c5ce0867f89b0
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-01-06 00:13:04 -0800

    client update

--- bugcord.py
+++ bugcord.py
@@ -7,18 +7,24 @@ async def client():
     uri = "ws://10.0.0.25:25987"
 
     client = await websockets.connect(uri)
+    await asyncio.gather(
+        consume_client(client),
+        produce(client)
+        )
 
-    async for message in client:
-        await consume_client(message)
-    await produce(client)
+    print("cloihg")
+    client.close()
 
-async def consume_client(message):
-    print(message)
+async def consume_client(websocket:websockets.WebSocketServerProtocol):
+    async for message in websocket:
+        print("resc " + message)
 
 async def produce(websocket:websockets.WebSocketServerProtocol):
     print("taking input")
     while True:
-        await websocket.send(input("message: "))
+        msg = await asyncio.to_thread(input)
+        print("msg: " + msg)
+        await websocket.send(msg)
 
 async def main():
     clientTask = asyncio.create_task(client())
