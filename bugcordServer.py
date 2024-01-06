d792bf9addc75cc54806fe8ad2807b7a9e308f17
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-01-05 21:44:24 -0700

    Added relay server

---
+++ bugcordServer.py
@@ -0,0 +1,40 @@
+import websockets
+import asyncio
+
+CONNECTIONS = set()
+
+async def server():
+    stop = asyncio.Future()
+    
+    server = await websockets.serve(handle, "10.0.0.25", port=25987)
+
+    await stop
+    await server.close()
+
+async def handle(websocket:websockets.WebSocketServerProtocol):
+    asyncio.gather(
+        register_connection(websocket),
+        consume_server(websocket)
+        )
+
+async def register_connection(websocket:websockets.WebSocketServerProtocol):
+    CONNECTIONS.add(websocket)
+    print("added connection")
+    try:
+        await websocket.wait_closed()
+    finally:
+        print("removed conectino")
+        CONNECTIONS.remove(websocket)
+
+async def consume_server(websocket:websockets.WebSocketServerProtocol):
+    print("echoiung")
+    async for message in websocket:
+        print("casting")
+        websockets.broadcast(CONNECTIONS, message)
+
+async def main():
+    await server()
+    
+
+if __name__ == '__main__':
+    asyncio.run(main())
