0cd15f42927dafc63fe7ce5e00d789423fc28135
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-01-06 00:37:09 -0700

    removed server from bugcord.py

--- bugcord.py
+++ bugcord.py
@@ -4,42 +4,13 @@ import asyncio
 CONNECTIONS = set()
 
 async def client():
-    uri = "ws://localhost:25987"
+    uri = "ws://10.0.0.25:25987"
 
     client = await websockets.connect(uri)
-    
+
     async for message in client:
         await consume_client(message)
     await produce(client)
-
-async def server():
-    stop = asyncio.Future()
-    
-    server = await websockets.serve(handle, "10.0.0.25", port=25987)
-
-    await stop
-    await server.close()
-
-async def handle(websocket:websockets.WebSocketServerProtocol):
-    asyncio.gather(
-        register_connection(websocket),
-        consume_server(websocket)
-        )
-
-async def register_connection(websocket:websockets.WebSocketServerProtocol):
-    CONNECTIONS.add(websocket)
-    print("added connection")
-    try:
-        await websocket.wait_closed()
-    finally:
-        print("removed conectino")
-        CONNECTIONS.remove(websocket)
-
-async def consume_server(websocket:websockets.WebSocketServerProtocol):
-    print("echoiung")
-    async for message in websocket:
-        print("casting")
-        websockets.broadcast(CONNECTIONS, message)
 
 async def consume_client(message):
     print(message)
@@ -50,12 +21,8 @@ async def produce(websocket:websockets.WebSocketServerProtocol):
         await websocket.send(input("message: "))
 
 async def main():
-    serverTask = asyncio.create_task(server())
-    #clientTask = asyncio.create_task(client())
-    if input("host? y/n ") == "y":
-        await serverTask
-    #else:
-        #await clientTask
+    clientTask = asyncio.create_task(client())
+    await clientTask
     
 
 if __name__ == '__main__':
