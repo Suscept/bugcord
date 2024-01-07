e55334ddab700a71f8c15365071c4dea485e07fa
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-01-06 19:05:13 -0700

    Added user system

--- bugcord.py
+++ bugcord.py
@@ -1,13 +1,45 @@
 import websockets
 import asyncio
 import json
+import uuid
 
-username = input("username: ")
+print("Welcome to Bugcord!")
+try:
+    userRaw = open("user.txt").read()
+except FileNotFoundError:
+    print("No user.txt found. Creating new account")
+    username = input("Enter username: ")
+    uri = input("Enter IP of server to connect to: ")
+    print("Creating account...")
+    user = {"username":username, "defaultConnect":uri, "secretDONTSHARE":str(uuid.uuid4())}
+    userRaw = json.dumps(user)
+    open("user.txt", "w").write(userRaw)
+    while True:
+        input("Created account!\nPlease close and reopen Bugcord.")
+    
+
+user = json.loads(userRaw)
+print("Logging in as " + user["username"])
+print("Connect to " + user["defaultConnect"] + "?\nPress enter to connect. Enter server IP to connect to other server")
+serverInput = input()
+if serverInput == "":
+    uri = user["defaultConnect"]
+else:
+    uri = serverInput
+    setDefault = input("Set this IP as your default? Y/N: ")
+    if setDefault == "y":
+        user["defaultConnect"] = uri
+        print("Set " + uri + " as default ip")
 
 async def client():
-    uri = "ws://10.0.0.25:25987"
+    uriParsed = uri.split(":")
+    if len(uriParsed) == 1:
+        uriParsed.append("25987")
 
-    client = await websockets.connect(uri)
+    formattedUri = "ws://{}:{}".format(uriParsed[0], uriParsed[1])
+    print("Connecting to " + formattedUri + "...")
+    client = await websockets.connect(formattedUri)
+    print("Connected!")
     await asyncio.gather(
         consume_client(client),
         produce(client)
@@ -22,7 +54,7 @@ async def consume_client(websocket:websockets.WebSocketServerProtocol):
         if msgJson["usr"] == username:
             continue
         if msgJson["pktpe"] == "msg":
-            print(msgJson["usr"] + msgJson["content"])
+            print(msgJson["usr"] + ": " + msgJson["content"])
 
 async def produce(websocket:websockets.WebSocketServerProtocol):
     print("taking input")
