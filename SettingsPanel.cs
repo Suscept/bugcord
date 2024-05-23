98643986cd44a1e1d717c4172122eab59409b101
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-05-22 19:54:19 -0600

    Initial settings window functionality

---
+++ SettingsPanel.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public partial class SettingsPanel : MarginContainer
+{
+	[Export] public LineEdit usernameSetting;
+	[Export] public TextureRect profilePictureDisplay;
+	[Export] public FileDialog profilePictureDialog;
+
+	private string pickedProfileImageDir;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{	
+		string selfPfpId = (string)Bugcord.clientUser["id"] + "-pfp";
+		string username = (string)Bugcord.clientUser["username"];
+		if (Bugcord.cacheIndex.ContainsKey(selfPfpId)){
+			DisplayProfileImage((string)Bugcord.cacheIndex[selfPfpId]);
+		}
+
+		usernameSetting.Text = username;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void Save(){
+		if (pickedProfileImageDir != null && pickedProfileImageDir.Length > 0){
+			Bugcord.PrepareEmbed(pickedProfileImageDir, (string)Bugcord.clientUser["id"] + "-pfp", false);
+		}
+
+		Bugcord.clientUser["username"] = usernameSetting.Text;
+		Bugcord.SaveUser();
+	}
+
+	public void PickProfileImage(){
+		profilePictureDialog.Popup();
+	}
+
+	public void OnProfileImagePicked(string dir){
+		pickedProfileImageDir = dir;
+		DisplayProfileImage(dir);
+	}
+
+	private void DisplayProfileImage(string path){
+		Image loadedImage = Image.LoadFromFile(path);
+		ImageTexture imageTexture = ImageTexture.CreateFromImage(loadedImage);
+
+		profilePictureDisplay.Texture = imageTexture;
+	}
+}
