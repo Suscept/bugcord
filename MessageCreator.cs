5073f3f92afadbf1fe0464be8e863b85c97f8de8
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-07-31 00:23:43 -0600

    Added drag and drop functionality for files

--- MessageCreator.cs
+++ MessageCreator.cs
@@ -19,6 +19,7 @@ public partial class MessageCreator : MarginContainer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetWindow().FilesDropped += EmbedFile;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
