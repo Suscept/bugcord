75f25da00222214b81cceb2710d9e642c32bb6f5
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-07-31 02:08:57 -0600

    Message window now properly autoscrolls
    This was not fun. https://www.reddit.com/r/godot/comments/qhbi8y/how_to_scroll_a_scrollcontainer_to_the_bottom/

--- MessageWindow.cs
+++ MessageWindow.cs
@@ -9,30 +9,34 @@ public partial class MessageWindow : Control
 	[Export] public VBoxContainer messageContainer;
 	[Export] public ScrollContainer scrollContainer;
 
+	private VScrollBar scrollBar;
+	private bool atBottom;
+	private double maxScroll;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		scrollBar = scrollContainer.GetVScrollBar();
+		scrollBar.Changed += OnContentAdded;
+		maxScroll = scrollBar.MaxValue;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
 	}
 
 	public void DisplayNewMessage(Dictionary message){
-		VScrollBar vScroll = scrollContainer.GetVScrollBar();
+		atBottom = IsScrollAtBottom();
 
 		MessageUI newMessage = messageScene.Instantiate<MessageUI>();
 		newMessage.Initiate(message);
 		messageContainer.AddChild(newMessage);
-
-		int trueMax = (int)(vScroll.MaxValue - vScroll.Page);
-		//scrollContainer.ScrollVertical = trueMax;
-		// idk bruh
 	}
 
 	public void DisplayNewMediaMessage(Dictionary message){
+		atBottom = IsScrollAtBottom();
+
 		MessageUI newMessage = messageScene.Instantiate<MessageUI>();
 		newMessage.InitiateMediaMode(message);
 		messageContainer.AddChild(newMessage);
@@ -40,5 +44,24 @@ public partial class MessageWindow : Control
 		newMessage.bugcord = bugcord;
 		bugcord.OnEmbedCached += newMessage.CacheUpdated;
 		bugcord.OnFileBufferUpdated += newMessage.FileBufferUpdated;
+	}
+	
+	public void OnContentAdded(){
+		if (maxScroll == scrollBar.MaxValue) // Needed since this function gets called multiple times for some reason causing unexpected behavior
+			return;
+
+		if (atBottom == true){ // Only autoscroll if the viewport is already at the bottom
+			ScrollToBottom();
+		}
+	}
+
+	public void ScrollToBottom(){
+		maxScroll = scrollBar.MaxValue;
+		scrollContainer.ScrollVertical = (int)maxScroll;
+	}
+
+	private bool IsScrollAtBottom(){
+		int scrollDiff = (int)scrollBar.MaxValue - (int)scrollContainer.Size.Y;
+		return scrollContainer.ScrollVertical == scrollDiff || scrollDiff < 0; // scrollDif will be < 0 when content is smaller than the scroll container
 	}
 }
