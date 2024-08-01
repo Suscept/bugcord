4d1bb2aa0a7b7376ea5c107f612dba042bf9b159
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-08-01 02:39:53 -0600

    Removed AES functions from bugcord script

--- Bugcord.cs
+++ Bugcord.cs
@@ -844,32 +844,6 @@ public partial class Bugcord : Node
 		return bytes;
 	}
 
-	public static byte[] AESEncrypt(byte[] plaintext, byte[] key, byte[] iv){
-		using (Aes aes = Aes.Create()){
-			aes.Key = key;
-			aes.IV = iv;
-			aes.Mode = CipherMode.CBC;
-			aes.Padding = PaddingMode.PKCS7;
-
-			using (ICryptoTransform encryptor = aes.CreateEncryptor()){
-				return encryptor.TransformFinalBlock(plaintext, 0, plaintext.Length);
-			}
-		}
-	}
-
-	public static byte[] AESDecrypt(byte[] cyphertext, byte[] key, byte[] iv){
-		using (Aes aes = Aes.Create()){
-			aes.Key = key;
-			aes.IV = iv;
-			aes.Mode = CipherMode.CBC;
-			aes.Padding = PaddingMode.PKCS7;
-
-			using (ICryptoTransform decryptor = aes.CreateDecryptor()){
-				return decryptor.TransformFinalBlock(cyphertext, 0, cyphertext.Length);
-			}
-		}
-	}
-
 	public static string ToBase64(byte[] data){
 		return Convert.ToBase64String(data);
 	}
