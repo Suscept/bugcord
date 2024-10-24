9b4112493cf0a58a79cf1268e7492fbc4c23aa1c
Susceptance <46062232+Suscept@users.noreply.github.com>
2024-10-24 04:58:19 -0600

    Fixed request for user's profile picture still being made if none is set

--- SettingsPanel.cs
+++ SettingsPanel.cs
@@ -17,7 +17,7 @@ public partial class SettingsPanel : MarginContainer
 		fileService = GetNode<FileService>("/root/Main/Bugcord/FileService");
 		userService = GetNode<UserService>("/root/Main/Bugcord/UserService");
 
-		if (userService.profilePictureFileId != null){
+		if (userService.profilePictureFileId != ""){
 			if (fileService.GetFile(userService.profilePictureFileId, out byte[] data)){
 				TryLoadProfilePicture();
 			}else{
